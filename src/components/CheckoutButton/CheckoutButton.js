6cb3fe552f83ea3cccc6205ac9fd8384c52808b4
heyDante <badalsaibo@gmail.com>
2020-04-26 18:18:15 +0530

    Intital cleanup/ Scaffolding and First component

---
+++ src/components/CheckoutButton/CheckoutButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CheckoutButton.scss';
+
+const CheckoutButton = () => {
+	return (
+		<button className='checkout-button'>checkout</button>
+	);
+};
+
+export default CheckoutButton;
